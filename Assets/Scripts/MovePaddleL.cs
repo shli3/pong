9e9ab030464317b5fc24e06c4b52226990898ca7
shli3 <shlimoonsteven@gmail.com>
2021-07-19 12:35:51 -0400

    Added mass to paddle ⚓🏓 paddles collide with bumpers and don't move when ball hits even with unfrozen Y

--- Assets/Scripts/MovePaddleL.cs
+++ Assets/Scripts/MovePaddleL.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class MovePaddleL : MonoBehaviour
 {
-    
+
     [SerializeField]
-    private float PlayerSpeed = 5.0f;
+    private float PlayerSpeed = 10.0f;
 
     void Update()
     {
         transform.position = new Vector3(transform.position.x, transform.position.y + (Input.GetAxis("Vertical") * PlayerSpeed * Time.deltaTime), transform.position.z);
     }
+
 }
